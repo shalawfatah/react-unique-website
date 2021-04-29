23c31cec828a1664baa3ea8cc2fe16a85e59234a
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-28 20:57:37 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
